e25ebfae0528d93e39e18a47c49e50b7aef50751
Zafer CAKMAK <mzcakmak@gmail.com>
2022-06-22 11:56:09 +0300

    adds greeting class and endpoint

--- src/main/java/com/grispi/bootcamp/restservice/GreetingController.java
+++ src/main/java/com/grispi/bootcamp/restservice/GreetingController.java
@@ -2,11 +2,23 @@ package com.grispi.bootcamp.restservice;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class GreetingController {
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+		Greeting g = new Greeting(counter.incrementAndGet(), String.format(template, name));
+
+		return g;
+	}
 
 	@GetMapping("/")
-	public String greeting(@RequestParam(value = "xyz") String xyz) {
+	public String hello(@RequestParam(value = "xyz") String xyz) {
 		return "Hello World xyz: " + xyz;
 	}
 
