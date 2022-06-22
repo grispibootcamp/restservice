d92dceb87690744948ba740baa697e5adaefa87e
Zafer CAKMAK <mzcakmak@gmail.com>
2022-06-22 11:37:42 +0300

    creates an example rest controller

---
+++ src/main/java/com/grispi/bootcamp/restservice/GreetingController.java
@@ -0,0 +1,17 @@
+package com.grispi.bootcamp.restservice;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class GreetingController {
+
+	@GetMapping("/")
+	public String greeting(@RequestParam(value = "xyz") String xyz) {
+		return "Hello World xyz: " + xyz;
+	}
+
+	@PostMapping("/")
+	public String post(@RequestBody String xyz) {
+		return "Hello World: " + xyz;
+	}
+}
