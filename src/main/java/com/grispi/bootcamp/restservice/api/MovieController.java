fefee0f204c193233757c4552222441d2aca653c
Hüseyin Doğukan Yılmaz <dogu_codeM@hotmail.com>
2022-06-29 11:31:04 +0300

    addMovie method(ResponseEntity)

--- src/main/java/com/grispi/bootcamp/restservice/api/MovieController.java
+++ src/main/java/com/grispi/bootcamp/restservice/api/MovieController.java
@@ -3,12 +3,14 @@ package com.grispi.bootcamp.restservice.api;
 import com.grispi.bootcamp.restservice.model.Movie;
 import com.grispi.bootcamp.restservice.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/movie/")
+@RequestMapping("api/v1/movies/")
 public class MovieController {
 
     private final MovieService movieService;
@@ -18,12 +20,13 @@ public class MovieController {
         this.movieService = movieService;
     }
 
-    @PostMapping ("addMovie")
-    public void addMovie(@RequestBody Movie movie) {
+    @PostMapping ("movie")
+    public ResponseEntity<Movie> addMovie(@RequestBody Movie movie) {
         movieService.addMovie(movie);
+        return ResponseEntity.status(HttpStatus.CREATED).body(movie);
     }
 
-    @GetMapping("getAll")
+    @GetMapping("")
     public List<Movie> getAllMovies() {
         return movieService.getAllMovie();
     }
