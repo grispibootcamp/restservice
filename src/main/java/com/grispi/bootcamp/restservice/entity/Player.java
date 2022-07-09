282a10ffef7887277b14406d83e8c63a9b668509
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-07-09 17:11:38 +0300

    Created Player entity.

---
+++ src/main/java/com/grispi/bootcamp/restservice/entity/Player.java
@@ -0,0 +1,39 @@
+package com.grispi.bootcamp.restservice.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Player {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    public Player() {
+    }
+
+    public Player(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
