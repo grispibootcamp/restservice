abae2bf542d00174cc58edb343c52b8ab45d57c4
Kinto2517 <ersin.yilmaz2517@gmail.com>
2022-07-09 17:18:52 +0300

    Created Movie Controller for get movies and players post players and movies.

--- src/main/java/com/grispi/bootcamp/restservice/controller/MovieController.java
+++ src/main/java/com/grispi/bootcamp/restservice/controller/MovieController.java
@@ -1,57 +1,84 @@
 package com.grispi.bootcamp.restservice.controller;
 
-import com.grispi.bootcamp.restservice.model.Movie;
+import com.grispi.bootcamp.restservice.entity.Movie;
+import com.grispi.bootcamp.restservice.entity.Player;
 import com.grispi.bootcamp.restservice.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
 
     @Autowired
-    private final MovieRepository movieRepository;
-
-    public MovieController(MovieRepository movieRepository) {
-        this.movieRepository = movieRepository;
-    }
+    private MovieRepository movieRepository;
 
     @GetMapping
-    public List<Movie> getMovies() {
+    public Iterable<Movie> getAllMovies() {
         return movieRepository.findAll();
     }
 
-
     @PostMapping
-    public ResponseEntity<Movie> addMovie(@RequestBody Movie movie) {
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
         movieRepository.save(movie);
-        return ResponseEntity.status(HttpStatus.CREATED).body(movie);
+        return new ResponseEntity<>(movie, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
+        Movie movieToUpdate = movieRepository.findById(id).orElse(null);
+        if (movieToUpdate == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        movieToUpdate.setName(movie.getName());
+        movieToUpdate.setImdbKey(movie.getImdbKey());
+        movieToUpdate.setGenres(movie.getGenres());
+        movieToUpdate.setPlayers(movie.getPlayers());
+        movieRepository.save(movieToUpdate);
+        return new ResponseEntity<>(movieToUpdate, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Movie> deleteMovie(@PathVariable Long id) {
+        Movie movieToDelete = movieRepository.findById(id).orElse(null);
+        if (movieToDelete == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        movieRepository.delete(movieToDelete);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Movie> getMovie(@PathVariable Long id) {
-        Optional<Movie> movie = movieRepository.findById(id);
-        if (movie.isPresent()) {
-            return ResponseEntity.ok(movie.get());
-        } else {
-            return ResponseEntity.notFound().build();
+        Movie movie = movieRepository.findById(id).orElse(null);
+        if (movie == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        return new ResponseEntity<>(movie, HttpStatus.OK);
     }
 
-    @GetMapping("/{id}/delete")
-    public ResponseEntity<Movie> deleteMovie(@PathVariable Long id) {
-        Optional<Movie> movie = movieRepository.findById(id);
-        if (movie.isPresent()) {
-            movieRepository.delete(movie.get());
-            return ResponseEntity.ok(movie.get());
-        } else {
-            return ResponseEntity.notFound().build();
+
+    @GetMapping("/{id}/players")
+    public ResponseEntity<Iterable<Player>> getMoviePlayers(@PathVariable Long id) {
+        Movie movie = movieRepository.findById(id).orElse(null);
+        if (movie == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
+        return new ResponseEntity<>(movie.getPlayers(), HttpStatus.OK);
     }
+
+    @PostMapping("/{id}/players")
+    public ResponseEntity<Movie> addPlayerToMovie(@PathVariable Long id, @RequestBody Player player) {
+        Movie movie = movieRepository.findById(id).orElse(null);
+        if (movie == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        movie.getPlayers().add(player);
+        movieRepository.save(movie);
+        return new ResponseEntity<>(movie, HttpStatus.OK);
+    }
+
+
 }
